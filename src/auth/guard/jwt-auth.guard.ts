358a8bf5bebc83a9e976de95b03aea1d5f3ab330
sjl <906320505@qq.com>
2023-02-10 18:22:55 +0800

    授权

--- src/auth/guard/jwt-auth.guard.ts
+++ src/auth/guard/jwt-auth.guard.ts
@@ -9,7 +9,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     constructor(private reflector: Reflector){
         super()
     }
-    canActivate(context:ExecutionContext , ){
+    canActivate(context:ExecutionContext){
         const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
             context.getHandler(),
             context.getClass(),
